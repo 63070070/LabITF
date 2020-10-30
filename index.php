bb483a64f8f9a051a75016053f862b4d53f9c71e
63070070 <73686792+63070070@users.noreply.github.com>
2020-10-30 15:21:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
-<?php
-echo "<h2>ITKMITL!</h2>";
-echo "Hello world!<br>";
-?>
+<html>
+    <head>
+        <title>63070070</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>นายธนโชติ ลักษณ์วิเศษดี ชื่อเล่น โอ๊ต</h1>
+        <p>My First Paragraph</p>
+        <img src="bg.jpg">
+        <br>
+        <img src="dog.jpg">
+        <br>
+        <a href="http://kmitl.ac.th/en">My university</a>
+        
+        <h2>THIS IS MY FAVOURITE FOOTBALL TEAM</h2>
+        <ul>
+            <li>Barcelona</li>
+            <li>Chelsea</li>
+            <li>Dortmund</li>
+        </ul>
+        <h3>เรื่องตลก</h3>
+        <ul>
+            <li>ไวพจน์ เพชรตะพึด.... เฮ้ย !!
+                ไวพจน์ พึดตะพิด .... เฮ้ย !!...
+                ไวพจน์ เพชรสุพรรณ ...เฮ้ยยย !!... ถูก แล้ว....</li>
+        </ul>
+    </body>
+</html>
